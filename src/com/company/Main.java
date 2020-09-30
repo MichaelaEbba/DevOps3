869c267a358dc8e599cdbf06d69f3eaa09cdc7de
Michaela Palmqvist <michaelapalmqvist@live.se>
2020-09-30 16:19:03 +0200

    Inlamningsuppgift 3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,30 @@
 package com.company;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-	System.out.println("");
-    }
+	public static void main(String[] args) {
+		boolean isRunning = true;
+
+		Scanner in = new Scanner(System.in);
+
+		while (isRunning) {
+			System.out.println("What is the meaning of life?");
+
+			try {
+				int operator = in.nextInt();
+				if (operator == 42) {
+					isRunning = false;
+					System.out.println("You know now the answer, bye bye");
+				}
+			} catch (InputMismatchException ex) {
+				System.out.println("There's been an error" + ex);
+				isRunning = false;
+			}
+
+			 System.out.println("Please answer 42");
+		}
+	}
 }
